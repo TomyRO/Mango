3f0705ae69d9d3ebcb7d384a979547c804854f36
irina <id611@ic.ac.uk>
2015-02-28 21:55:45 +0100

    initial api

--- functii.py
+++ functii.py
@@ -1,21 +1,52 @@
 #!usr/bin/python
 
 # primit de la aplicatie
-def upload_req():
-  return 0
+def upload_req(file):
+  filename = extract_filename(file)
+  size = get_size(file)
+
+  start_slice, start = get_curr_offset()
+  end = start + size
+
+  chunks = split(start, get_size(file), file)
+
+  return upload_storage(chunks, filename)
 
 # primit de la aplicatie
-def download_req():
-  return 0
+def download_req(filename):
+  chunks_start, chunks_end = get_chunks_offsets(fileID)
+  chunk_numbers = get_chunk_numbers(fileID)
+
+  # request la tomi pentru chunks
+  chunks = [] #request la tomi
+
+  return merge_chunks(chunks, chunks_start, chunks_end)
 
 # trimis lui tomi
-def upload_storage():
-  return 0
-
-# trimis lui tomi
-def download_storage():
-  return 0
+def upload_storage(chunks, filename):
+  files = generate_files(chunks)
+  # call la tomi cu files
 
 # request lui tomi
-def list_storage():
-  return 0
+def list_storage(userID):
+  # din tabel local
+  return get_file_names()
+
+# 
+def split(start_offset, size, file):
+  chunks = []
+
+  # check if file fits in one chunk only
+  offset = get_curr_offset()
+  initial_chunk = req_chunk(offset)
+
+  # returneaza unde a ramas in fisier
+  # ret -1 daca a terminat
+  file_offset = fill_initial_chunk(initial_chunk, file)
+
+  chunks.append(initial_chunk)
+
+  # append all other chunks 
+
+  return chunks
+
