461176b195e380c0f70375b2a942df31d336497b
irina <id611@ic.ac.uk>
2015-02-28 21:15:45 +0100

    initial api

---
+++ functii.py
@@ -0,0 +1,21 @@
+#!usr/bin/python
+
+# primit de la aplicatie
+def upload_req():
+  return 0
+
+# primit de la aplicatie
+def download_req():
+  return 0
+
+# trimis lui tomi
+def upload_storage():
+  return 0
+
+# trimis lui tomi
+def download_storage():
+  return 0
+
+# request lui tomi
+def list_storage():
+  return 0
