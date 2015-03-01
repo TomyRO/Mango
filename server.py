fdf9c30ac343591a1f93eef5c2b17b0d337454d6
Alex Tomescu <alex.dan.tomescu@gmail.com>
2015-03-01 05:12:49 +0100

    Added Dropbox upload/download

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ urls = (
     '/googleauth', 'googleauth',
     '/googleauthcallback', 'googleauthcallback',
     '/listfiles', 'listfiles',
+    '/testing', 'testing',
     '/(.*)', 'hello'
 )
 
@@ -32,82 +33,108 @@ session = web.session.Session(app, web.session.DiskStore('sessions'), initialize
 class hello:        
     def GET(self, name):
 
-	data = web.input(code=None)
+  data = web.input(code=None)
 
-	flow = dropbox.client.DropboxOAuth2Flow(app_key, app_secret, "https://146.148.7.87:80/", session, "dd")
+  flow = dropbox.client.DropboxOAuth2Flow(app_key, app_secret, "https://146.148.7.87:80/", session, "dd")
 
         if not name: 
             name = 'World'
-	
+  
         if data.code:
             url = "https://api.dropbox.com/1/oauth2/token"
 
             access_token, user_id, state_url = flow.finish({"state": session.dd, "code": data.code}) 
-	    session.access_token = access_token
-	    session.user_id = user_id
+      session.access_token = access_token
+      session.user_id = user_id
 
-	    web.seeother('/listfiles')
-	else:
-	    return flow.start()
-	
+      web.seeother('/listfiles')
+  else:
+      return flow.start()
+  
         return 'Hello, ' + name + '!'
 
 class listfiles:
     def GET(self):
-    	
- 	client = dropbox.client.DropboxClient(session.access_token)
-	return client.metadata('/')	
+      
+  client = dropbox.client.DropboxClient(session.access_token)
+  return client.metadata('/') 
 
 class googleauth:
     def GET(self):
-	# Authorize server-to-server interactions from Google Compute Engine.
-	
-	flow = flow_from_clientsecrets('./client_secrets.json',
+  # Authorize server-to-server interactions from Google Compute Engine.
+  
+  flow = flow_from_clientsecrets('./client_secrets.json',
                                scope='https://www.googleapis.com/auth/drive',
                                redirect_uri='https://mangocloud.me:80/googleauthcallback')
 
-	service = build('drive', 'v2')
+  service = build('drive', 'v2')
 
-	def retrieve_all_files(service):
+  def retrieve_all_files(service):
             result = []
             page_token = None
             while True:
-     	        try:
-      		    param = {}
-      		    if page_token:
-        		param['pageToken'] = page_token
-      		    files = service.files().list(**param).execute()
+              try:
+              param = {}
+              if page_token:
+            param['pageToken'] = page_token
+              files = service.files().list(**param).execute()
 
-      		    result.extend(files['items'])
-      		    page_token = files.get('nextPageToken')
+              result.extend(files['items'])
+              page_token = files.get('nextPageToken')
       
-        	    break
+              break
     
-		except errors.HttpError, error:
-      		    print 'An error occurred: %s' % error
+    except errors.HttpError, error:
+              print 'An error occurred: %s' % error
                     break
             return result
 
-	if 'authed' in session:
-	    http = httplib2.Http()
-	    credentials = session.credentials;	    
-	    http = credentials.authorize(http)
-	    service = build('drive', 'v2', http)
+  if 'authed' in session:
+      http = httplib2.Http()
+      credentials = session.credentials;      
+      http = credentials.authorize(http)
+      service = build('drive', 'v2', http)
 
-  	    return retrieve_all_files(service)
-  	else:
-	    uri = flow.step1_get_authorize_url()
-	    return uri    
+      return retrieve_all_files(service)
+  else:
+    uri = flow.step1_get_authorize_url()
+    return uri    
 
 
 class googleauthcallback:
     def GET(self):
-	flow = flow_from_clientsecrets('./client_secrets.json', scope='https://www.googleapis.com/auth/drive', redirect_uri='https://mangocloud.me:80/googleauthcallback')
-	if 'code' in web.input():
-	    credentials = flow.step2_exchange(web.input()['code'])
-	    session.authed = True
-	    session.credentials = credentials
-	    web.seeother('/googleauth')
+      flow = flow_from_clientsecrets('./client_secrets.json', scope='https://www.googleapis.com/auth/drive', redirect_uri='https://mangocloud.me:80/googleauthcallback')
+      if 'code' in web.input():
+          credentials = flow.step2_exchange(web.input()['code'])
+          session.authed = True
+          session.credentials = credentials
+          web.seeother('/googleauth')
+
+
+class dropbox_worker:
+  def __init__(self, access_token):
+    self.client = dropbox.client.DropboxClient(access_token)
+
+  def upload(self, filename, array):
+    f = open("tmp.dat", "w");
+    f.write(array)
+    f.close()
+    f = open("tmp.dat", "r");
+    self.client.put_file(filename, f)
+
+  def download(self, filename):
+    f, metadata = client.get_file_and_metadata(filename)
+    return f.read()
+
+class google_worker:
+  pass
+
+class testing:
+  def GET(self):
+    array = [123,124,125,126] * 10
+    d = dropbox_worker(session.access_token)
+    d.upload("00.dat", array)
+    return str(d.download("00.dat"))
 
 if __name__ == "__main__":
     app.run()
